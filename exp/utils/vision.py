a4eeed49e7cb8938d0483c9fafc42df13335b01a
DigitalBotLab <yizhou_zhao@berkeley.edu>
2024-01-24 14:00:07 -0800

    update ocr

--- exp/utils/vision.py
+++ exp/utils/vision.py
@@ -1,6 +1,9 @@
 import pyautogui
 from PIL import Image
 
+from paddleocr import PaddleOCR
+ocr = PaddleOCR(use_gpu=True) # use_angle_cls=True, lang='en'
+
 def capture_and_crop(x1, y1, x2, y2):
     '''
     Capture the screen and crop the image using PIL
