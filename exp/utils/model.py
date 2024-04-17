bb5cb138c3dd2ecc3c5c45c99dcf93a3a934ef70
Yuning Yang <yuningyang@g.ucla.edu>
2024-04-17 13:26:41 -0700

    Model Part2
    Interest word + Spatial Strategy for both Gemini-pro-vision + GPT-4V

--- exp/utils/model.py
+++ exp/utils/model.py
@@ -10,13 +10,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 import base64
 
+
+#Encode Image
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-    
-    
-    
+#API Key Setup    
 config = dotenv_values(".env")
 print("-----------------[gemini] config", config)
 genai.configure(api_key=config["GOOGLE_API_KEY"])
@@ -25,12 +25,19 @@ os.environ['GOOGLE_API_KEY'] = config["GOOGLE_API_KEY"]
 #   text = text.replace('•', '  *')
 #   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-#
+#Image Path
 image_path = 'C:/Users/madis/LLM4SocialMedia/examples/left_half.jpeg'
 img = encode_image(image_path)
 
-llm = ChatGoogleGenerativeAI(model='gemini-pro-vision')
-prompt = "Please point out there is a cat inside the image or not and describe the image."
+#Spatial Strategy Prompt Template --Gemini
+Interest = "pet"
+textgemi = genai.GenerativeModel('gemini-pro')
+prompt1 = f"I am scrolling on TikTok, I am an illiterate person who is interested in {Interest}. Can you come up with 5 visual content related but is not {Interest} I would want to watch?"
+response1 = textgemi.generate_content(prompt)
+print(response1.text)
+
+gemi = ChatGoogleGenerativeAI(model='gemini-pro-vision')
+prompt2 = "Please point out there is a cat inside the image or not and describe the image."
 message = HumanMessage(
     content=[
         {'type':'text','text':prompt},
@@ -38,15 +45,29 @@ message = HumanMessage(
     ]
 )
         
-         
-response = llm.invoke([message])
-print(response.content)
+response2 = gemi.invoke([message])
+print(response2.content)
 
-
+#Spatial Strategy Prompt Template --GPT4 https://platform.openai.com/docs/models/continuous-model-upgrades==GPT4 Version lists
 base64_image = encode_image(image_path)
 from openai import OpenAI
-client = OpenAI(api_key =config["OPENAI_KEY"] )
-response = client.chat.completions.create(
+client = OpenAI(api_key =config["OPENAI_KEY"])
+response3 = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": prompt1},
+            ],
+        }
+    ],
+    max_tokens=300,
+)
+print(response3.choices[0].message.content)
+
+# client = OpenAI(api_key =config["OPENAI_KEY"])
+response4 = client.chat.completions.create(
     model="gpt-4-vision-preview",
     messages=[
         {
@@ -63,5 +84,5 @@ response = client.chat.completions.create(
     max_tokens=300,
 )
 
-print(response.choices[0].message.content)
+print(response4.choices[0].message.content)
 
